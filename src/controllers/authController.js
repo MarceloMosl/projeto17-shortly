5d7485cb016e88011ef8ada96448651555f6f863
MarceloMosl <marcelomosl@icloud.com>
2023-02-24 22:20:28 -0300

    Feat: Singin

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 import { db } from "../config/database.js";
+import { v4 as uuid } from "uuid";
 import bcrypt from "bcrypt";
 
 export async function signUp(req, res) {
@@ -19,6 +20,44 @@ export async function signUp(req, res) {
       [name, email, passwordEncript]
     );
     return res.sendStatus(201);
+  } catch (error) {
+    return res.send(error);
+  }
+}
+
+export async function signIn(req, res) {
+  const { email, password } = req.body;
+
+  const userCheck = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  if (userCheck.rows.length === 0) return res.sendStatus(409);
+  if (!bcrypt.compareSync(password, userCheck.rows[0].password))
+    return res.sendStatus(409);
+
+  try {
+    const token = uuid();
+
+    const regularUser = await db.query(
+      `SELECT * FROM sessions WHERE "userId" = $1`,
+      [userCheck.rows[0].id]
+    );
+
+    if (regularUser.rows.length === 0) {
+      await db.query('insert into sessions ("userId", token) values ($1,$2)', [
+        userCheck.rows[0].id,
+        token,
+      ]);
+      return res.status(200).send({ token });
+    }
+
+    await db.query('UPDATE sessions SET token = $1 WHERE "userId" = $2', [
+      token,
+      userCheck.rows[0].id,
+    ]);
+
+    return res.status(200).send({ token });
   } catch (error) {
     return res.send(error);
   }
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signUp } from "../controllers/authController.js";
+import { signIn, signUp } from "../controllers/authController.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { schemaUser } from "../schemas/signUpSchema.js";
 
 const authRoute = Router();
 
 authRoute.post("/users", validateSchema(schemaUser), signUp);
+authRoute.post("/singin", signIn);
 
 export default authRoute;
